5d741ad1eb84efbf662ab2f38796175483c71e13
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 20:09:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,12 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+  suite_a: "Amanda Presley", 
+  suite_b: "Seymour Hoffman", 
+  suite_c: "Alfred Tennyson", 
+  suite_d: "Charlie Chaplin", 
+  suite_e: "Crumpet the Elf"
+}
 
+# Collect name of passenger who is staying in Suite A and who's name beings with "A"
 def select_winner(passengers)
-  # add the code snippet here!
+  
 end
