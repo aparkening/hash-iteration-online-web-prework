2301b576cb89f95be033f1cc643e256b69293d19
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 20:14:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,7 @@ birthday_kids = {
 
 # Take in array of kid names and birthdays and puts a special message
 def happy_birthday(birthday_kids)
-  
-  
-  
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
-
-
