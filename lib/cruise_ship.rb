9d4b669d7824314d5d172ed5090693d6a16fcb22
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 20:09:47 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,10 @@ passengers = {
 
 # Collect name of passenger who is staying in Suite A and who's name beings with "A"
 def select_winner(passengers)
-  
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
 end
